ca8b0bf287e84b8c9f282b5fd539172bad06558a
alizamir25 <ali0786zamir@gmail.com>
2024-09-28 12:38:05 +0100

    Patch

--- app.js
+++ app.js
@@ -9,21 +9,21 @@ dragula([
       el.className.replace("ex-moved", "");
     })
     .on("drop", function (el){
-      el.className += "ex-moved";
+      el.className+="ex-moved";
     })
     .on("over", function (el, container){
-      container.className += "ex-over";
+      container.className+="ex-over";
     })
     .on("out", function (el, container){
       container.className.replace("ex-over", "");
     });
   
   function addTask() {
-    var inputTask = document.getElementById("taskText").value;
-    document.getElementById("to-do").innerHTML +=
-      "<li class='task'><p>" + inputTask + "</p></li>";
-    document.getElementById("taskText").value = "";
+    var inputTask=document.getElementById("taskText").value;
+    document.getElementById("to-do").innerHTML+=
+      "<li class='task'><p>"+inputTask+"</p></li>";
+    document.getElementById("taskText").value="";
   }
   function emptyTrash() {
-    document.getElementById("trash").innerHTML = "";
+    document.getElementById("trash").innerHTML="";
   }
